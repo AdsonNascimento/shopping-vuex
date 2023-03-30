f44086a026d916f04ab3220dd42aa8d458e89c7b
Adson Nascimento <adson.nascimento.112@ufrn.edu.br>
2023-03-29 21:43:12 -0300

    erro in viwes home

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,18 @@ import axios from 'axios';
 
 export default createStore({
   state: {
-    products: []
+    products: [],
+    productsInBag: []
   },
   getters: {
   },
   mutations: {
     loadProducts(state, products) {
       state.products = products;
+    },
+
+    addToBag(state, product) {
+      state.productsInBag.push(product);
     }
   },
   actions: {
@@ -19,6 +24,10 @@ export default createStore({
         .then(response => {
           commit('loadProducts', response.data)
         })
+    },
+
+    addToBag({ commit }, product) {
+      commit('addToBag', product);
     }
   },
   modules: {
